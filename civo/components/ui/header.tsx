505c40d8d99cac8bba37f68b2349ae65aff63368
yeonhj0507 <yeonhj0507@naver.com>
2025-05-15 13:16:03 +0900

    feat:splash headerfooter hidden

--- civo/components/ui/header.tsx
+++ civo/components/ui/header.tsx
@@ -5,12 +5,13 @@ import Image from 'next/image';
 import Logo from '@/src/img/logo.png'
 import Help from '@/src/img/help.png'
 import Notice from '@/src/img/notice.png'
+import path from "path";
 
 export default function Header() {
     const pathname = usePathname();
 
     return (
-    <header className={`fixed top-0 w-full pt-[4px] pb-[4px] z-50 bg-white ${pathname==='/home'?'drop-shadow-[0_2px_4px_rgba(0,0,0,0.25)]':''}`}>
+    <header className={`fixed top-0 w-full pt-[4px] pb-[4px] z-50 bg-white ${pathname==='/home'?'drop-shadow-[0_2px_4px_rgba(0,0,0,0.25)]':''} ${pathname==='/splash1'||pathname==='/splash2'?'hidden':''}`}>
     <div className="mt-[54.65px] w-full text-[23.12px] flex items-center gap-[10px]">
       <Image src={Logo} alt="로고" width={21} height={19} className="ml-[158.37px] mt-[7px] mb-[7px]"/>
       <div className="flex items-center">CIVO</div>
