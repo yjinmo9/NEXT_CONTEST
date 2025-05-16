adae73be5c8674cde042604c687778f3091c66ca
yeonhj0507 <yeonhj0507@naver.com>
2025-05-16 17:27:09 +0900

    fix:header align

--- civo/components/ui/header.tsx
+++ civo/components/ui/header.tsx
@@ -13,10 +13,12 @@ export default function Header() {
     return (
     <header className={`fixed top-0 w-full pt-[4px] pb-[4px] z-50 bg-white ${pathname==='/home'?'drop-shadow-[0_2px_4px_rgba(0,0,0,0.25)]':''} ${pathname==='/splash1'||pathname==='/splash2'?'hidden':''}`}>
     <div className="mt-[54.65px] w-full text-[23.12px] flex items-center gap-[10px]">
-      <Image src={Logo} alt="로고" width={21} height={19} className="ml-[158.37px] mt-[7px] mb-[7px]"/>
-      <div className="flex items-center">CIVO</div>
+      <div className="mx-auto flex items-center">
+        <Image src={Logo} alt="로고" width={21} height={19} className="mt-[7px] mb-[7px]"/>
+        <div className="flex items-center">CIVO</div>
+      </div>
       {pathname==='/home'?(
-      <div className="ml-[70px] flex gap-[8px]">
+      <div className="absolute mr-[10px] flex gap-[8px]">
         <Image src={Help} alt="도움말" width={21} height={19} />
         <Image src={Notice} alt="소식" width={21} height={19} />
       </div>):(
